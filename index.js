6ff126276dc42ab63e460b50693cf64a09f99e53
Patrick <patrickkgibbs@gmail.com>
2024-08-18 10:38:19 -0600

    required path and delted hello world test GET request

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
-app.use(express.static('dist'))
+app.use(express.static('dist'));
+app.use(cors());
 
 let notes = [
-    {
-        id: '1',
-        content: 'HTML is easy',
-        important: true,
-    },
-    {
-        id: '2',
-        content: 'Browser can execute only JavaScript',
-        important: false,
-    },
-    {
-        id: '3',
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        important: true,
-    },
+    
 ];
 
 const requestLogger = (req, res, next) => {
